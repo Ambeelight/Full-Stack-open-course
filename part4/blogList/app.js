db6dd3d722c88fcdfc30be62dbd2b7224b91edb9
Makar Istratov <imakar98@gmail.com>
2023-10-11 03:21:48 +0300

    Completed exercises 4.1-4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MONGODB_URI } = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
