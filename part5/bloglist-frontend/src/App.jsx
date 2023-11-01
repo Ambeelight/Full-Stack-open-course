102e062bc5d22a9d35b9f57ef1a0ad1bed6bc6ec
Makar Istratov <imakar98@gmail.com>
2023-11-01 03:13:24 +0200

    Completed exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -167,9 +167,11 @@ const App = () => {
       <button onClick={handleLogout}>logout</button>
       <h2>create new</h2>
       {blogForm()}
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} likeHandler={likeHandler} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} likeHandler={likeHandler} />
+        ))}
     </div>
   )
 }
