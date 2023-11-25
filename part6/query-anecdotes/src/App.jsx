68637865913b895bd956349a4d5e4cc0c268663f
Makar Istratov <imakar98@gmail.com>
2023-11-26 00:23:06 +0200

    Completed exercises 6.20-6.22

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,38 +1,58 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './anecdotes'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateVotes = useMutation({
+    mutationFn: updateAnecdote,
+    onMutate: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(
+        ['anecdotes'],
+        anecdotes.map((a) => (a.id === updatedAnecdote.id ? updatedAnecdote : a))
+      )
+    },
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateVotes.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false,
+    refetchOnWindowFocus: false,
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) return <div>Loading...</div>
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  console.log('Anecdotes', result)
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
