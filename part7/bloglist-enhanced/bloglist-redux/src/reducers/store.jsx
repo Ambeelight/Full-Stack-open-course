23bdade2dcee92a52dfb6cebe7217c4ef3c992b9
Ambeelight <imakar98@gmail.com>
2024-01-27 02:50:36 +0200

    Updated redux store

--- part7/bloglist-enhanced/bloglist-redux/src/reducers/store.jsx
+++ part7/bloglist-enhanced/bloglist-redux/src/reducers/store.jsx
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './notificationReducer'
 import blogReducer from './blogReducer'
+import loginReducer from './loginReducer'
+import userReducer from './userReducer'
 
 const store = configureStore({
   reducer: {
     notification: notificationReducer,
     blogs: blogReducer,
+    login: loginReducer,
+    users: userReducer,
   },
 })
 
