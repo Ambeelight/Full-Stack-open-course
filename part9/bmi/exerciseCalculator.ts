12397bdfe06ffa908a5188b26429ff8e8a016161
Ambeelight <imakar98@gmail.com>
2024-03-27 22:55:42 +0200

    Completed exercises 9.2-9.3

---
+++ part9/bmi/exerciseCalculator.ts
@@ -0,0 +1,80 @@
+interface TrainingValues {
+  exerciseHours: number[];
+  target: number;
+}
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const parseExerciseArguments = (args: Array<string>): TrainingValues => {
+  if (args.length < 10) throw new Error('Not enough arguments');
+  if (args.length > 10) throw new Error('Too many arguments');
+
+  const trainingHours = args.slice(2, 9).map(Number);
+
+  if (trainingHours.some(isNaN) || isNaN(Number(args[9]))) {
+    throw new Error('Provided values were not numbers!');
+  } else {
+    return {
+      exerciseHours: trainingHours,
+      target: Number(args[9]),
+    };
+  }
+};
+
+const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): Result => {
+  const periodLength = exerciseHours.length;
+  const trainingDays = exerciseHours.filter((hours) => hours > 0).length;
+  const average =
+    exerciseHours.reduce((sum, curr) => sum + curr, 0) / periodLength;
+  const success = target >= average;
+  const getRating = (average: number, target: number): number => {
+    if (average < target) {
+      return 1;
+    } else if (average === target) {
+      return 2;
+    } else {
+      return 3;
+    }
+  };
+  const rating = getRating(average, target);
+  const getRatingDescription = (rating: number): string => {
+    if (rating === 1) {
+      return 'not too bad but could be better';
+    } else if (rating === 2) {
+      return 'nicely done! You achieved the target';
+    } else return 'you are machine, not a human';
+  };
+  const ratingDescription = getRatingDescription(rating);
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+try {
+  const { exerciseHours, target } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(exerciseHours, target));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
