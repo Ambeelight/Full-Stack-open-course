cdb12bd720b12c32e7037ab26c5b024468f65df1
Ambeelight <imakar98@gmail.com>
2024-04-04 01:59:52 +0300

    Completed exercise 9.16

--- part9/flight-diaries/flight-diary-frontend/src/App.tsx
+++ part9/flight-diaries/flight-diary-frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { DiaryEntry } from './types';
+import { getAllDiaries, createDiary } from './services/diaries';
 
 function App() {
-  return <></>;
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+  useEffect(() => {
+    getAllDiaries().then((data) => setDiaries(data));
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {diaries.map((diary) => (
+          <li key={diary.id}>
+            {diary.date} {diary.visibility} {diary.weather} {diary.comment}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
