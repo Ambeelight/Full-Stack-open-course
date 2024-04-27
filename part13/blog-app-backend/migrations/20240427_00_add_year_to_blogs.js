264ad242faf05dd766d249402e8e82da8bf304e1
Ambeelight <imakar98@gmail.com>
2024-04-27 20:56:31 +0300

    Completed exercises 13.17-13.18

---
+++ part13/blog-app-backend/migrations/20240427_00_add_year_to_blogs.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+
+const up = async ({ context: queryInterface }) => {
+  await queryInterface.addColumn('blogs', 'year', {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1991,
+      max: new Date().getFullYear(),
+      isWthitinRange(value) {
+        if (value < 1991 || value > new Date().getFullYear()) {
+          throw new Error('Year must be between 1991 and the current year');
+        }
+      },
+    },
+  });
+};
+
+const down = async ({ context: queryInterface }) => {
+  await queryInterface.removeColumn('blogs', 'year');
+};
+
+export { up, down };
--- part13/blog-app-backend/util/db.js
+++ part13/blog-app-backend/util/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
+import { Umzug, SequelizeStorage } from 'umzug';
 
 config();
 
@@ -13,16 +14,41 @@ const sequelize = new Sequelize(
   }
 );
 
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigration = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
 const connectToDB = async () => {
   try {
     await sequelize.authenticate();
+    await runMigration();
     console.log('connected to the database');
   } catch (error) {
-    console.log('failed to connect to the database');
+    console.log('failed to connect to the database', error);
     return process.exit(1);
   }
 
   return null;
 };
 
-export { connectToDB, sequelize };
+export { connectToDB, sequelize, rollbackMigration };
